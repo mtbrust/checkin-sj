e42d55a6ac7445f289c924dc27f78333de874d65
Mateus Brust <61816139+mtbrust@users.noreply.github.com>
2024-05-22 22:20:17 -0300

    Foto ok

--- src/js/crop-image.js
+++ src/js/crop-image.js
@@ -1,9 +1,10 @@
 
 
 async function capture() {
+    return true;
     console.log("iniciou a captura.");
     const video = document.querySelector("#video");
-    const img = document.getElementById("screenshots");
+    let img = document.getElementById("screenshots");
     const canvas = document.createElement("canvas");
 
     // Obtém as dimensões em formato 4:5
@@ -20,56 +21,8 @@ async function capture() {
     canvas.width = dimensoes.w;
     canvas.height = dimensoes.h;
     canvas.getContext("2d").drawImage(video, dimensoes.x, dimensoes.y);
-    
-    printToFile(document.querySelector(".boxvideo"));
 
     // exibe a imagem.
-    img.src = canvas.toDataURL("image/png");
-}
-
-function dimensiona(w, h, aspectRatio = 4/5) {
-
-    // get the aspect ratio of the input image
-    const inputImageAspectRatio = w / h;
-
-    // if it's bigger than our target aspect ratio
-    let outputWidth = w;
-    let outputHeight = h;
-    if (inputImageAspectRatio > aspectRatio) {
-        outputWidth = h * aspectRatio;
-    } else if (inputImageAspectRatio < aspectRatio) {
-        outputHeight = h / aspectRatio;
-    }
-
-    // calculate the position to draw the image at
-    const outputX = (outputWidth - w) * .5;
-    const outputY = (outputHeight - h) * .5;
-
-    return {
-        x: outputX,
-        y: outputY,
-        w: outputWidth,
-        h: outputHeight
-    };
-}
-
-function printToFile(div) {
-    html2canvas(div, {
-        onrendered: function (canvas) {
-            var myImage = canvas.toDataURL("image/png");
-            //create your own dialog with warning before saving file
-            //beforeDownloadReadMessage();
-            //Then download file
-            downloadURI("data:" + myImage, "yourImage.png");
-        }
-    });
-}
-
-function downloadURI(uri, name) {
-    var link = document.createElement("a");
-    link.download = name;
-    link.href = uri;
-    link.click();
-    //after creating link you should delete dynamic link
-    //clearDynamicLink(link); 
+    img.src = canvas.toDataURL("image/png");    
+    printToFile(img);
 }
--- src/js/script_cam.js
+++ src/js/script_cam.js
@@ -1,11 +1,10 @@
-(function () {
+
   if (
     !"mediaDevices" in navigator ||
     !"getUserMedia" in navigator.mediaDevices
   ) {
     alert("Camera API is not available in your browser");
-    return;
-  }
+  }else{
 
   // get page elements
   const video = document.querySelector("#video");
@@ -13,9 +12,7 @@
   const btnPause = document.querySelector("#btnPause");
   const btnScreenshot = document.querySelector("#btnScreenshot");
   const btnChangeCamera = document.querySelector("#btnChangeCamera");
-  const screenshotsContainer = document.querySelector("#screenshots");
-  const canvas = document.querySelector("#canvas");
-  const devicesSelect = document.querySelector("#devicesSelect");
+  let img = document.getElementById("screenshots");
 
   // video constraints
   const constraints = {
@@ -47,7 +44,7 @@
     $(btnPlay).css({
       display: "none"
     });
-    
+
     $(btnPause).css({
       display: "block"
     });
@@ -56,32 +53,47 @@
   // pause
   btnPause.addEventListener("click", function () {
     video.pause();
-    
+
     $(btnPlay).css({
       display: "block"
     });
-    
+
     $(btnPause).css({
       display: "none"
     });
   });
 
-  // take screenshot
+  // take screenshots
   btnScreenshot.addEventListener("click", function () {
-    const img = document.createElement("img");
-    canvas.width = video.videoWidth;
-    canvas.height = video.videoHeight;
-    canvas.getContext("2d").drawImage(video, 0, 0);
-    img.src = canvas.toDataURL("image/png");
-    screenshotsContainer.prepend(img);
+    funcScreenshot()
   });
+
+  function funcScreenshot() {
+    let canvas = document.createElement("canvas");
+
+    // Obtém as dimensões em formato 4:5
+    dimensoes = dimensiona(video.videoWidth, video.videoHeight, 4 / 5);
+
+    ajustaCam(dimensoes);
+
+    canvas.width = dimensoes.w;
+    canvas.height = dimensoes.h;
+    canvas.getContext("2d").drawImage(video, dimensoes.x, dimensoes.y);
+
+    // exibe a imagem.
+    img.src = canvas.toDataURL("image/png");
+    printToFile(img);
+  }
 
   // switch camera
   btnChangeCamera.addEventListener("click", function () {
-    useFrontCamera = !useFrontCamera;
-
-    initializeCamera();
+    funcChangeCamera();
   });
+
+  function funcChangeCamera() {
+    useFrontCamera = !useFrontCamera;
+    initializeCamera();
+  }
 
   // stop video stream
   function stopVideoStream() {
@@ -100,10 +112,66 @@
     try {
       videoStream = await navigator.mediaDevices.getUserMedia(constraints);
       video.srcObject = videoStream;
+      // Espera video carregar na tela para redimensionar.
+      video.addEventListener("loadeddata", () => {
+        if (video.readyState >= 4) {
+          ajustaCam();
+        }
+      });
     } catch (err) {
       alert("Could not access the camera");
     }
   }
 
+  function ajustaCam() {
+
+    // video = document.querySelector("#video");
+
+    // Obtém as dimensões em formato 4:5
+    dimensoes = dimensiona(video.videoWidth, video.videoHeight, 4 / 5);
+
+    // Ajusta exibição da camera.
+    $(video).css({
+      marginLeft: -100,
+      width: dimensoes.x * -1
+    });
+  }
+
+  function dimensiona(w, h, aspectRatio = 4 / 5) {
+
+    // get the aspect ratio of the input image
+    const inputImageAspectRatio = w / h;
+
+    // if it's bigger than our target aspect ratio
+    let outputWidth = w;
+    let outputHeight = h;
+    if (inputImageAspectRatio > aspectRatio) {
+      outputWidth = h * aspectRatio;
+    } else if (inputImageAspectRatio < aspectRatio) {
+      outputHeight = h / aspectRatio;
+    }
+
+    // calculate the position to draw the image at
+    const outputX = (outputWidth - w) * .5;
+    const outputY = (outputHeight - h) * .5;
+
+    return {
+      x: outputX,
+      y: outputY,
+      w: outputWidth,
+      h: outputHeight
+    };
+  }
+
+  function printToFile(div) {
+    html2canvas(div).then(function (canvas) {
+      // Mandar a imagem via post aqui.
+      //console.log(canvas.toDataURL("image/png"));
+      console.log('Mandar a imagem via post aqui.')
+    });
+  }
+
   initializeCamera();
-})();
+
+    // return;
+  }
