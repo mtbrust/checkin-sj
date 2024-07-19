b47e2d8a842c2a3b0939d4dcd054a0735daaade1
Mateus Brust <contato@desv.com.br>
2024-07-19 07:28:28 -0300

    Ajuste foto

--- src/html/pesquisa.php
+++ src/html/pesquisa.php
@@ -7,6 +7,15 @@ if (isset($_GET['f-pesquisa']) && $_GET['f-pesquisa'] != '') {
     $resultado = $BdVisitantes->pesquisar($_GET['f-pesquisa']);
 }
 
+$user = Seguranca::getSession();
+$ids = Seguranca::getIdsAdmins();
+
+$show = 'd-none';
+
+if (in_array($user['id'], $ids)) {
+  $show = '';
+}
+
 ?>
 
 <div class="container my-4">
@@ -25,13 +34,26 @@ if (isset($_GET['f-pesquisa']) && $_GET['f-pesquisa'] != '') {
         } else {
             foreach ($resultado as $key => $value) {
         ?>
-                <div class=" col-sm-3 col-6 mt-3">
+                <div class=" col-sm-4 col-12 mt-3">
                     <div class="box_statistica shadow-sm">
-                        <a href="<?php echo BASE_URL . '?page=cadastro_editar&id=' . $value['id']; ?>">
+                        <a class="<?php echo $show; ?>" href="<?php echo BASE_URL . '?page=cadastro_editar&id=' . $value['id']; ?>">
                             <i class="fas fa-user-edit"></i>
                         </a>
-                        <h6 class="text-truncate"><?php echo $value['fullName']; ?></h6>
-                        <h1>-</h1>
+                        <h4 class="text-truncate"><?php echo $value['fullName']; ?></h4>
+                        <div class="row">
+                            <div class="col-4 pe-1">
+                                <img src="<?php echo $value['foto']; ?>" alt="" class="rounded" width="100%">
+                            </div>
+                            <div class="col-8 ps-1">
+                                <small style="font-size: 12px;">
+                                    Pulseira: <?php echo $value['pulseira'] . ' [' . $value['tpulseira'] .']'; ?>
+                                    <br>Telefone: <?php echo $value['telefone']; ?>
+                                    <br>Nascimento: <?php echo $value['nascimento']; ?>
+                                    <br><i class="fas fa-user-plus"></i> <?php echo $value['dtCreate']; ?>
+                                    <br><i class="fas fa-user-edit"></i> <?php echo $value['dtUpdate']; ?>
+                                </small>
+                            </div>
+                        </div>
                     </div>
                 </div>
         <?php
