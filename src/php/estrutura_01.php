2eaccdf2cd606c6bf3cf648d470aba11d1846ea5
Mateus Brust <contato@desv.com.br>
2026-07-15 17:05:22 -0300

    Ajuste favicon

--- src/php/estrutura_01.php
+++ src/php/estrutura_01.php
@@ -32,10 +32,15 @@ $page_name = BASE_NAME . ' - ' . $page;
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo $page_name; ?></title>
 
-    <link href="src/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="src/css/foto3.css">
-    <link rel="stylesheet" href="src/css/sweetalert2.min.css">
-    <link rel="stylesheet" href="src/css/default-min.css">
+    <link rel="icon" href="<?php echo BASE_URL; ?>favicon.ico" sizes="any">
+    <link rel="icon" type="image/svg+xml" href="<?php echo BASE_URL; ?>favicon.svg">
+    <link rel="icon" type="image/png" href="<?php echo BASE_URL; ?>favicon.png">
+    <link rel="apple-touch-icon" href="<?php echo BASE_URL; ?>favicon.png">
+
+    <link href="<?php echo BASE_URL; ?>src/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="<?php echo BASE_URL; ?>src/css/foto3.css">
+    <link rel="stylesheet" href="<?php echo BASE_URL; ?>src/css/sweetalert2.min.css">
+    <link rel="stylesheet" href="<?php echo BASE_URL; ?>src/css/default-min.css">
 </head>
 <body>
     <?php
@@ -51,16 +56,15 @@ $page_name = BASE_NAME . ' - ' . $page;
 
     ?>
 
-    <script src="src/js/jquery.min.js"></script>
-    <script src="src/js/jquery.mask.min.js"></script>
+    <script src="<?php echo BASE_URL; ?>src/js/jquery.min.js"></script>
+    <script src="<?php echo BASE_URL; ?>src/js/jquery.mask.min.js"></script>
     <script>window.SITE_BASE_URL = '<?php echo BASE_URL; ?>';</script>
-    <script src="src/js/default-min.js"></script>
-    <script src="src/js/visitante-popup.js"></script>
-    <script src="src/js/mask-min.js"></script>
-    <script src="src/js/bootstrap.bundle.min.js"></script>
-    <!-- <script src="src/js/script_cam.js"></script> -->
-    <script src="src/js/fontawesome.js"></script>
-    <script src="src/js/html2canvas.min.js"></script>
-    <script src="src/js/sweetalert2.min.js"></script>
+    <script src="<?php echo BASE_URL; ?>src/js/default-min.js"></script>
+    <script src="<?php echo BASE_URL; ?>src/js/visitante-popup.js"></script>
+    <script src="<?php echo BASE_URL; ?>src/js/mask-min.js"></script>
+    <script src="<?php echo BASE_URL; ?>src/js/bootstrap.bundle.min.js"></script>
+    <script src="<?php echo BASE_URL; ?>src/js/fontawesome.js"></script>
+    <script src="<?php echo BASE_URL; ?>src/js/html2canvas.min.js"></script>
+    <script src="<?php echo BASE_URL; ?>src/js/sweetalert2.min.js"></script>
 </body>
 </html>
